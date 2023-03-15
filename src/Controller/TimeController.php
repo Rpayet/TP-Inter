5c1f2270533ff46d6fe72ddd09b72ed576166ccc
Robin Payet <robinpayet@gmail.com>
2023-03-15 14:59:38 +0100

    Symfony Creation + BDD + Ex01 Intro

---
+++ src/Controller/TimeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TimeController extends AbstractController
+{
+    #[Route('/time', name: 'app_time')]
+    public function index(): Response
+    {
+        return $this->render('time/index.html.twig', [
+            'controller_name' => 'TimeController',
+        ]);
+    }
+}
