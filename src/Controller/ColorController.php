7e55a31f64448059f95ba72343e485237443b24e
Robin Payet <robinpayet@gmail.com>
2023-03-15 15:54:59 +0100

    Color page Done

--- src/Controller/ColorController.php
+++ src/Controller/ColorController.php
@@ -3,14 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ColorController extends AbstractController
 {
+
     #[Route('/color', name: 'app_color')]
     public function index(): Response
     {
-        return $this->render('color/index.html.twig');
+        return $this->render('color/index.html.twig', [
+            'colors' => ['blue', 'red'],
+        ]);
+    }
+
+    #[Route('/color/{color}', name: 'app_show')]
+    public function show($color): Response
+    {
+        return $this->render('color/show.html.twig', [
+            'color' => $color,
+        ]);
     }
 }
+
