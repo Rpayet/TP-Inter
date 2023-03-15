a70123e99b57138cf7e6b7d75991d22d95e2e38f
Robin Payet <robinpayet@gmail.com>
2023-03-15 16:26:03 +0100

    Timer done

--- src/Controller/TimeController.php
+++ src/Controller/TimeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,18 @@ class TimeController extends AbstractController
     #[Route('/time', name: 'app_time')]
     public function index(): Response
     {
+        
         return $this->render('time/index.html.twig');
+    }
+
+    #[Route('/time/now', name: 'app_show')]
+    public function show(): Response
+    {
+
+        $timer = new DateTime();
+
+        return $this->render('time/show.html.twig', [
+            'timer' => $timer->format('d/m/Y H:i:m'),
+        ]);
     }
 }
