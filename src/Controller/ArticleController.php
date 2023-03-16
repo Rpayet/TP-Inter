412c4cfbdc73bc6f899a77def105522a0424d1e2
Rpayet <robinpayet@gmail.com>
2023-03-16 14:50:00 +0100

    Création du tableau associatif + Vue

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,20 @@ class ArticleController extends AbstractController
 {
     protected $items = [];
 
-    public function
+    public function __construct()
+    {
+        $this->items = [
+            ['title' => 'Mug', 'text' => 'Disfrutar un buen café', 'publishedAt' => new DateTime(), 'author' => 'Billy'],
+            ['title' => 'Chair', 'text' => 'Take a good sit', 'publishedAt' => new DateTime(), 'author' => 'Paul'],
+            ['title' => 'Pillow', 'text' => 'Have a good naptime', 'publishedAt' => new DateTime(), 'author' => 'Franck'],
+        ];
+    }
+
     #[Route('/article', name: 'app_article')]
     public function index(): Response
     {
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'items' => $this->items,
         ]);
     }
 }
