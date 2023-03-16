87dc107be372367cda06958f5b7fc8291bb605e8
Rpayet <robinpayet@gmail.com>
2023-03-16 16:12:58 +0100

    BrainFuck Sur le Get et le Request

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+            'search' => $request->get('search'),
         ]);
     }
+
+    #[Route('/search/show', name: 'app_result_show')]
+    public function show(Request $request): Response
+    {
+        return $this->render('search/show.html.twig');
+    }
+
 }
