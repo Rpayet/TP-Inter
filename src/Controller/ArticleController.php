786a243c8ccbed6025adfaec555243c765373374
Rpayet <robinpayet@gmail.com>
2023-03-16 14:22:41 +0100

    Creation des controller Article et Search

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    protected $items = [];
+
+    public function
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+}
